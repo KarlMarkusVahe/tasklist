cde2ed1425f7a4c375e64b47073706d76cc7d931
KarlMarkus <karl-markus.vaher@voco.ee>
2022-10-06 15:37:12 +0300

    good task list

--- app.js
+++ app.js
@@ -1,36 +1,46 @@
-const form = document.querySelector("form")
-const taskinput = document.querySelector("#task")
+const form  = document.querySelector('form')
+const taskInput = document.querySelector('#task')
 const taskList = document.querySelector('ul')
 const delAllTasks = document.querySelector('#del-tasks')
 
-form.addEventListener("submit", addTask)
+form.addEventListener('submit', addTask)
 taskList.addEventListener('click', deleteTask)
-delAllTasks.addEventListener('click', deleteallTasks)
-document.addEventListener('DOMContentLoaded')
+delAllTasks.addEventListener('click', deleteAllTasks)
+document.addEventListener('DOMContentLoaded', getTasks)
 
 function addTask(e){
-    console.log(taskinput.value)
-    const li = document.createElement("li")
-    li.appendChild(document.createTextNode(taskinput.value))
-    li.className = "collection-item"
-    const a = document.createElement("a")
+    // create list item
+    const li = document.createElement('li')
+    li.appendChild(document.createTextNode(taskInput.value))
+    li.className = 'collection-item'
+    const a = document.createElement('a')
     a.appendChild(document.createTextNode('X'))
-    a.className = "blue-text text-darken-2 secondary-content"
-    a.setAttribute("href", "#")
+    a.className = 'blue-text text-darken-2 secondary-content'
+    a.setAttribute('href', '#')
     li.appendChild(a)
-    const ul = document.querySelector("ul")
+    // add to list
+    const ul = document.querySelector('ul')
     ul.appendChild(li)
-    addTaskLS(taskinput.value)
-    taskinput.value = ''
+    addTaskLS(taskInput.value)
+    taskInput.value = ''
     e.preventDefault()
 }
 
-function deleteTask(e) {
+function deleteTask(e){
     if(e.target.textContent == 'X'){
-        if(confirm('yous sure'))
+        if(confirm('Are you sure to delete this task?')){
             e.target.parentElement.remove()
+            deleteTaskLS(e.target.parentElement.textContent.slice(0,-1))
+        }
     }
+}
 
+function deleteAllTasks(e){
+    //taskList.innerHTML = ''
+    while (taskList.firstChild){
+        taskList.removeChild(taskList.firstChild)
+    }
+    localStorage.removeItem('tasks')
 }
 
 function addTaskLS(task) {
@@ -38,7 +48,7 @@ function addTaskLS(task) {
     if(localStorage.getItem('tasks') === null){
         tasks = []
     } else {
-        tasks = JSON.parse(localStorage.getItem(('tasks')))
+        tasks = JSON.parse(localStorage.getItem('tasks'))
     }
     tasks.push(task)
     localStorage.setItem('tasks', JSON.stringify(tasks))
@@ -49,12 +59,71 @@ function deleteTaskLS(task) {
     if(localStorage.getItem('tasks') === null){
         tasks = []
     } else {
-        tasks = JSON.parse(localStorage.getItem(('tasks')))
+        tasks = JSON.parse(localStorage.getItem('tasks'))
     }
-    tasks.forEach((taskLS), taskIndex) => {
-        if (taskLS === task){
+    tasks.forEach((taskLS, taskIndex) => {
+        if(taskLS === task){
             tasks.splice(taskIndex, 1)
         }
     })
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+}
 
+function getTasks(){
+    let tasks
+    if(localStorage.getItem('tasks') === null){
+        tasks = []
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'))
+    }
+    tasks.forEach((task) => {
+        const li = document.createElement('li')
+        li.appendChild(document.createTextNode(task))
+        li.className = 'collection-item'
+        const a = document.createElement('a')
+        a.appendChild(document.createTextNode('X'))
+        a.className = 'blue-text text-darken-2 secondary-content'
+        a.setAttribute('href', '#')
+        li.appendChild(a)
+        // add to list
+        const ul = document.querySelector('ul')
+        ul.appendChild(li)
+    })
+}
+function filter(e){
+    var input, filter1, ul, li, a, i, txtvalue;
+    input = document.getElementById('task1');
+    filter1 = input.value.toUpperCase();
+    ul = document.getElementById('minu');
+    li = ul.getElementsByTagName('li');
+
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("")[0];
+        txtvalue = a.textContent || a.innerText;
+        if (txtvalue.toUpperCase().indexOf(filter1) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+        //e.preventDefault()
+    }
+}
+function myFunction() {
+    // Declare variables
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById('myInput');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName('li');
+
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
 }
