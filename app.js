f7fc06644a6410baa22b74e2d08a2de93976c9a6
KarlMarkus <karl-markus.vaher@voco.ee>
2022-10-11 15:08:42 +0300

    good task list

--- app.js
+++ app.js
@@ -80,6 +80,10 @@ function getTasks(){
         const li = document.createElement('li')
         li.appendChild(document.createTextNode(task))
         li.className = 'collection-item'
+        const b = document.createElement("b")
+        b.appendChild(document.createTextNode(task))
+        b.style.display = "none"
+        li.appendChild(b)
         const a = document.createElement('a')
         a.appendChild(document.createTextNode('X'))
         a.className = 'blue-text text-darken-2 secondary-content'
@@ -98,7 +102,7 @@ function filter(e){
     li = ul.getElementsByTagName('li');
 
     for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("")[0];
+        a = li[i].getElementsByTagName("b")[0];
         txtvalue = a.textContent || a.innerText;
         if (txtvalue.toUpperCase().indexOf(filter1) > -1) {
             li[i].style.display = "";
@@ -106,24 +110,5 @@ function filter(e){
             li[i].style.display = "none";
         }
         //e.preventDefault()
-    }
-}
-function myFunction() {
-    // Declare variables
-    var input, filter, ul, li, a, i, txtValue;
-    input = document.getElementById('myInput');
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("myUL");
-    li = ul.getElementsByTagName('li');
-
-    // Loop through all list items, and hide those who don't match the search query
-    for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("a")[0];
-        txtValue = a.textContent || a.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
-        } else {
-            li[i].style.display = "none";
-        }
     }
 }
